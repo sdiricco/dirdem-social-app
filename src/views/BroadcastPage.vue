347f23f071db328e21401d545756450f581580c4
Simone <sdiricco.91@gmail.com>
2023-02-23 12:33:29 +0100

    update bcast page

--- src/views/BroadcastPage.vue
+++ src/views/BroadcastPage.vue
@@ -1,26 +1,25 @@
 <template>
-  
   <h1>Benvenuto {{ userEmail }}</h1>
   <p>Id: {{ userId }}</p>
   <ion-modal :is-open="modalOpen">
-      <ion-header>
-        <ion-toolbar>
-          <ion-buttons slot="start">
-            <ion-button @click="modalOpen = false">Cancel</ion-button>
-          </ion-buttons>
-          <ion-title>Welcome</ion-title>
-          <ion-buttons slot="end">
-            <ion-button :strong="true" @click="onSubimit">Confirm</ion-button>
-          </ion-buttons>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content class="ion-padding">
-        <ion-item>
-          <ion-label position="stacked">Enter your name</ion-label>
-          <ion-input ref="input" type="text" placeholder="Your name"></ion-input>
-        </ion-item>
-      </ion-content>
-    </ion-modal>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-button @click="modalOpen = false">Cancel</ion-button>
+        </ion-buttons>
+        <ion-title>Welcome</ion-title>
+        <ion-buttons slot="end">
+          <ion-button :strong="true" @click="onSubimit">Confirm</ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ion-item>
+        <ion-label position="stacked">Enter your name</ion-label>
+        <ion-input ref="input" type="text" placeholder="Your name"></ion-input>
+      </ion-item>
+    </ion-content>
+  </ion-modal>
   <ion-fab slot="fixed" vertical="bottom" horizontal="end">
     <ion-fab-button @click="modalOpen = true">
       <ion-icon :icon="add"></ion-icon>
@@ -30,30 +29,51 @@
 
 <script lang="ts" setup>
 import { onMounted, ref } from "vue";
-import { IonContent, IonHeader, IonPage, IonModal, IonToolbar, IonButtons, IonFab, IonFabButton, IonIcon,IonItem, IonLabel, IonInput, IonButton, IonTitle } from "@ionic/vue";
-import {supabaseProjectId} from "../constants"
-import { add } from 'ionicons/icons';
-import db from "../api/api"
-const userEmail = ref('')
-const userId = ref('')
-const modalOpen = ref(false)
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonModal,
+  IonToolbar,
+  IonButtons,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonTitle,
+} from "@ionic/vue";
+import { supabaseProjectId } from "../constants";
+import { add } from "ionicons/icons";
+import db from "../api/api";
+const userEmail = ref("");
+const userId = ref("");
+const modalOpen = ref(false);
 
-async function onSubimit(){
+async function onSubimit() {
   modalOpen.value = false;
-  await db.bcast.insert(userId.value)({maxDistanceKm: 100,expiresAt: new Date(), tag: ['mioTag'], location: {lat: 11.11, lng: 22.22}, maxUsers: 30, content: {
-    message: "message",
-    title: "title",
-  }   })
+  await db.bcast.insert(userId.value)({
+    maxDistanceKm: 100,
+    expiresAt: new Date(),
+    tag: ["mioTag"],
+    location: { lat: 11.11, lng: 22.22 },
+    maxUsers: 30,
+    content: {
+      message: "message",
+      title: "title",
+    },
+  });
 }
 
-onMounted(async ()=>{
-  const authKey = `sb-${supabaseProjectId}-auth-token`
-  const userData = JSON.parse(localStorage.getItem(authKey) || "")
-  userEmail.value = userData?.user?.email
-  userId.value = userData?.user?.id
-  console.log("userData", userData)
-  const response = await db.bcast.getPending(userId.value)
-  console.log(response)
-})
-
+onMounted(async () => {
+  const authKey = `sb-${supabaseProjectId}-auth-token`;
+  const userData = JSON.parse(localStorage.getItem(authKey) || "");
+  userEmail.value = userData?.user?.email;
+  userId.value = userData?.user?.id;
+  console.log("userData", userData);
+  const response = await db.bcast.getPending(userId.value);
+  console.log(response);
+});
 </script>
