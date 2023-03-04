3fe86e798cc7baf2174a61c050176b6b022d09ef
Simone <sdiricco.91@gmail.com>
2023-03-04 19:02:36 +0100

    add some style to profile

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,31 +1,43 @@
 <template>
-  <div class="d-flex justify-content-center">
-    <ion-avatar>
-      <img :src="avatarUrl" />
-    </ion-avatar>
+  <div class="p-4">
+    <div class="d-flex flex-column justify-content-center align-items-center pb-4">
+      <ion-avatar class="mb-4 border">
+        <img :src="avatarUrl" />
+      </ion-avatar>
+      <ion-card-title class="mb-4">{{ user.name }}</ion-card-title>
+
+      <ion-button shape="round" fill="outline" color="primary" class="mb-4">
+      <ion-icon slot="start" :icon="pencil" ></ion-icon>
+      Modifica Profilo 
+    </ion-button>
+    </div>
+
+
+
+    <ion-list>
+      <ion-item lines="none">  
+        <ion-icon slot="start" :icon="mailOutline"></ion-icon>
+        <ion-label>
+          <h3>Email</h3>
+          <p>{{ user.email }}</p>
+        </ion-label>
+      </ion-item>
+      <ion-item lines="none">
+        <ion-icon slot="start" :icon="callOutline"> </ion-icon>
+        <ion-label>
+          <h3>Telefono</h3>
+          <p>{{ user.phone }}</p>
+        </ion-label>
+      </ion-item>
+      <ion-item lines="none">
+        <ion-icon slot="start" :icon="lockClosedOutline"></ion-icon>
+        <ion-label>
+          <h3>Password</h3>
+          <p>**********</p>
+        </ion-label>
+      </ion-item>
+    </ion-list>
   </div>
-  <ion-card-title>{{ user.name }}</ion-card-title>
-  <ion-card-subtitle>{{ user.status }}</ion-card-subtitle>
-
-  <ion-list>
-    <ion-item>
-      <ion-icon slot="start" :icon="mailOutline"></ion-icon>
-      <ion-label>Email</ion-label>
-      <ion-label>{{ user.email }}</ion-label>
-    </ion-item>
-    <ion-item>
-      <ion-icon slot="start" :icon="callOutline">  </ion-icon>
-      <ion-label>Telefono</ion-label>
-      <ion-label>{{ user.phone }}</ion-label>
-    </ion-item>
-    <ion-item>
-      <ion-icon slot="start" :icon="lockClosedOutline"></ion-icon>
-      <ion-label>Password</ion-label>
-      <ion-label>**********</ion-label>
-    </ion-item>
-  </ion-list>
-
-  <ion-button expand="block" color="primary"> Modifica Profilo </ion-button>
 </template>
 
 <script lang="ts" setup>
@@ -46,7 +58,7 @@ import {
   IonCardTitle,
   IonCardSubtitle,
 } from "@ionic/vue";
-import { mailOutline, callOutline, lockClosedOutline  } from "ionicons/icons";
+import { mailOutline, callOutline, lockClosedOutline, pencil } from "ionicons/icons";
 import { useProfileStore } from "@/store/profile";
 import { onMounted, reactive, ref } from "vue";
 const profileStore = useProfileStore();
