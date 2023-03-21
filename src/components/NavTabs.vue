99ce4e83dcaeb26563513d5bd3a1339d3780ce81
Simone <sdiricco.91@gmail.com>
2023-03-21 16:14:49 +0100

    refactoring yet

---
+++ src/components/NavTabs.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-tab-bar slot="bottom">
+    <ion-tab-button tab="overview" href="/home/candidate-broadcasts">
+      <ion-label>Candidate</ion-label>
+    </ion-tab-button>
+
+    <ion-tab-button tab="categories" href="/home/joined-broadcasts">
+      <ion-label>Joined</ion-label>
+    </ion-tab-button>
+  </ion-tab-bar>
+</template>
+
+<script lang="ts" setup>
+import { IonTabBar, IonTabButton, IonLabel } from "@ionic/vue";
+
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,22 +5,15 @@
         <NavBar />
       </ion-header>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="overview" href="/home/broadcast">
-          <ion-label>Home</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="categories" href="/home/profile">
-          <ion-label>Profile</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
+      <NavTabs />
     </ion-tabs>
   </ion-page>
 </template>
 
 <script lang="ts" setup>
 import NavBar from "@/components/NavBar.vue";
-import { IonHeader, IonPage, IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonLabel } from "@ionic/vue";
+import { IonHeader, IonPage, IonTabs, IonRouterOutlet } from "@ionic/vue";
+import NavTabs from "@/components/NavTabs.vue";
 import { useMainStore } from "@/store";
 
 const store = useMainStore();
